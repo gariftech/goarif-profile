bca65cb49cc07a89edfe5fe06aebb43983ff61b1
masb0ymas <n.fajri@outlook.com>
2025-01-09 10:31:42 +0700

    chore: update deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -112,6 +112,22 @@ export default {
             'background-position': '0% 0%',
           },
         },
+        marquee: {
+          from: {
+            transform: 'translateX(0)',
+          },
+          to: {
+            transform: 'translateX(calc(-100% - var(--gap)))',
+          },
+        },
+        'marquee-vertical': {
+          from: {
+            transform: 'translateY(0)',
+          },
+          to: {
+            transform: 'translateY(calc(-100% - var(--gap)))',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
@@ -120,6 +136,8 @@ export default {
         ripple: 'ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite',
         'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
         shine: 'shine var(--duration) infinite linear',
+        marquee: 'marquee var(--duration) infinite linear',
+        'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
       },
     },
   },
