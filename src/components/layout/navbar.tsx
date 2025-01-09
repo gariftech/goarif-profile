f8105104f08b08ecd14bf1e9ca78405e0285ec4c
masb0ymas <n.fajri@outlook.com>
2025-01-09 12:40:35 +0700

    fix: link logo

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -104,7 +104,9 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
-            <span className="text-xl font-bold">Go Arif</span>
+            <Link href="/" className="text-xl font-bold">
+              Go Arif
+            </Link>
           </div>
 
           <div className="hidden md:flex items-center">
