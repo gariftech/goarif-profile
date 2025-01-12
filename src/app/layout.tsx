2619bdd961ecf8720dc83a6ef74e810c63c54a13
masb0ymas <n.fajri@outlook.com>
2025-01-12 19:51:13 +0700

    feat: add logo font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,32 @@
 import './globals.css'
 
+import clsx from 'clsx'
 import type { Metadata } from 'next'
 import { Nunito_Sans } from 'next/font/google'
+import localFont from 'next/font/local'
 import Provider from '~/lib/provider'
 
 const NunitoSans = Nunito_Sans({
   subsets: ['latin'],
   weight: ['200', '300', '400', '500', '600', '700', '800'],
+})
+
+const NeviaSans = localFont({
+  variable: '--font-NeviaSans',
+  src: [
+    {
+      path: '../../public/assets/fonts/nevia/nevia-light.woff',
+      weight: '300',
+    },
+    {
+      path: '../../public/assets/fonts/nevia/nevia-regular.woff',
+      weight: '400',
+    },
+    {
+      path: '../../public/assets/fonts/nevia/nevia-black.woff',
+      weight: '500',
+    },
+  ],
 })
 
 export const metadata: Metadata = {
@@ -21,7 +41,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${NunitoSans.className} antialiased`}>
+      <body className={clsx(NunitoSans.className, NeviaSans.variable, `antialiased`)}>
         <Provider>{children}</Provider>
       </body>
     </html>
